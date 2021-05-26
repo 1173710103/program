41b074f76d9b9816e8ab24f32bbade506c1d7e8a
1173710103 <45929782+1173710103@users.noreply.github.com>
2021-05-26 21:47:57 +0800

    add data

--- demo.py
+++ demo.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 
 
 X = []
+'''
 #读取人脸图像
 for i in range(9):
     filepath = 'C:/Users/10981/Desktop/bishe/test/face/' + str(i+1) +'.jpg'
@@ -18,12 +19,33 @@ for i in range(9):
     R = ir/ib
     x = [round(R,2),round(hg,2),round(hr,2),round(hb,2)]
     X.append(x)
+'''
+x = [1.40,190,108,90]
+X.append(x)
+x = [1.41,190,108,90]
+X.append(x)
+x = [1.43,196,112,95]
+X.append(x)
+x = [1.45,197,115,95]
+X.append(x)
+x = [1.51,199,117,99]
+X.append(x)
+x = [1.52,200,120,99]
+X.append(x)
+x = [1.57,204,128,105]
+X.append(x)
+x = [1.59,205,130,106]
+X.append(x)
+x = [1.63,209,140,110]
+X.append(x)
+x = [1.65,209,142,110]
+X.append(x)
     
 U,E,VT = np.linalg.svd(X)
 
 simple2=LinearRegression()
 x_train = X
-y_train = [97,98,97,99,98,98,98,98,98]
+y_train = [93,93,94,94,95,95,96,96,97,97]
 simple2.fit(x_train,y_train)
 
 p = []
